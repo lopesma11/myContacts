66a6595f45a792e133096e0da76a54d6d9891fe3
lopesma11 <ml722090@gmail.com>
2025-01-28 11:48:09 -0300

    task: creating update route

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -19,12 +19,62 @@ class ContactController {
         response.json(contact);
     }
 
-    store() {
-        // Criar um novo registro
+    async store(request, response) {
+        const { name, email, phone, category_id } = request.body;
+
+        if (!name) {
+            return response.status(400).json({ error: `Name is required` });
+        }
+
+        const contactExists = await ContactRepository.findByEmail(email);
+
+        if (contactExists) {
+            return response
+                .status(400)
+                .json({ error: `This e-mail is already in use` });
+        }
+
+        const contact = await ContactRepository.create({
+            name,
+            email,
+            phone,
+            category_id,
+        });
+
+        response.json(contact);
     }
 
-    update() {
-        // Editar um novo registro
+    async update(request, response) {
+        const { id } = request.params;
+
+        const { name, email, phone, category_id } = request.body;
+
+        const contactExists = await ContactRepository.findById(id);
+
+        if (!contactExists) {
+            return response.status(404).json({ error: `User not found` });
+        }
+
+        if (!name) {
+            return response.status(400).json({ error: `Name is required` });
+        }
+
+        const contactByEmail = await ContactRepository.findByEmail(email);
+
+        if (contactByEmail && contactByEmail.id !== id) {
+            return response
+                .status(400)
+                .json({ error: `This e-mail is already in use` });
+        }
+
+        const contact = await ContactRepository.update(id, {
+            name,
+            email,
+            phone,
+            category_id,
+        });
+
+        response.json(contact);
     }
 
     async delete(request, response) {
--- src/app/repositories/ContactRepository.js
+++ src/app/repositories/ContactRepository.js
@@ -41,6 +41,46 @@ class ContactRepository {
             resolve();
         });
     }
+
+    findByEmail(email) {
+        return new Promise((resolve, reject) => {
+            resolve(contacts.find((contact) => contact.email === email));
+        });
+    }
+
+    create({ name, email, phone, category_id }) {
+        return new Promise((resolve, reject) => {
+            const newContact = {
+                id: uuid(),
+                name,
+                email,
+                phone,
+                category_id,
+            };
+
+            contacts.push(newContact);
+
+            resolve(newContact);
+        });
+    }
+
+    update(id, { name, email, phone, category_id }) {
+        return new Promise((resolve, reject) => {
+            const updatedContact = {
+                id: uuid(),
+                name,
+                email,
+                phone,
+                category_id,
+            };
+
+            contacts = contacts.map((contact) => {
+                contact.id === id ? updatedContact : contact;
+            });
+
+            resolve(updatedContact);
+        });
+    }
 }
 
 module.exports = new ContactRepository();
