c9a20c58b392967a1a0e857f20abc17b58651044
lopesma11 <ml722090@gmail.com>
2025-02-01 12:04:38 -0300

    task: capture and dealing with errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("express-async-errors");
 const routes = require("./routes");
 const app = express();
 
@@ -6,4 +7,8 @@ app.use(express.json());
 
 app.use(routes);
 
+app.use((error, request, response, next) => {
+    console.log(error);
+    response.sendStatus(500);
+});
 app.listen(3000, () => console.log(`Server started at http://localhost:3000`));
