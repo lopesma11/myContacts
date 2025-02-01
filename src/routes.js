d30a5cc627dcc9cd634da2cbb655c61db1c9be1c
lopesma11 <ml722090@gmail.com>
2025-02-01 11:16:49 -0300

    task: creating Category Module (Controller, Repository and Route)

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const ContactController = require("./app/controllers/ContactController");
+const CategoryController = require("./app/controllers/CategoryController");
 const router = Router();
 
 router.get("/contacts", ContactController.index);
@@ -7,5 +8,11 @@ router.get("/contacts/:id", ContactController.show);
 router.delete("/contacts/:id", ContactController.delete);
 router.post("/contacts", ContactController.store);
 router.put("/contacts/:id", ContactController.update);
+
+router.get("/categories", CategoryController.index);
+router.post("/categories", CategoryController.store);
+router.get("/categories/:id", CategoryController.show);
+router.put("/categories/:id", CategoryController.update);
+router.delete("/categories/:id", CategoryController.delete);
 
 module.exports = router;
